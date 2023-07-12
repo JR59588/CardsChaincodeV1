21fe1f4bc9993dd52b72e50cf9fbe435fe46045c
PrajuBhoite <p.bhoite@zensar.com>
2023-07-12 12:16:42 +0530

    updated copyConnectionFiles

--- Back-end/copyConnectionFiles.sh
+++ Back-end/copyConnectionFiles.sh
@@ -1,5 +1,7 @@
 cp ../hlf-cards/organizations/peerOrganizations/AAD.hlfcards.com/connection-AAD.json ./
 cp ../hlf-cards/organizations/peerOrganizations/AAD.hlfcards.com/connection-AOD.json ./
 cp ../hlf-cards/organizations/peerOrganizations/AAD.hlfcards.com/connection-ACD.json ./
+cp ../hlf-cards/organizations/peerOrganizations/merchantOrg1.hlfcards.com/connection-org1.json ./
+cp ../hlf-cards/organizations/peerOrganizations/merchantOrg2.hlfcards.com/connection-org2.json ./
 
 
