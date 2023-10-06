4ed4f556e6ad6ebb15f88fd5ab868a1f04f9e817
abhiroop99z <abhiroop.b@zensar.com>
2023-10-06 16:47:44 +0530

    changes to event listener and added viewtx popup

--- Back-end/event-listener/application-js/eventListener.js
+++ Back-end/event-listener/application-js/eventListener.js
@@ -18,20 +18,14 @@ const io = socketIo(server, {
 
 const channelName = 'channel1';
 
-const org1MSP = 'Org1MSP';
-const Org1UserId = 'appOrg1User13';
-
-const orgPSPMSP = 'PSPMSP';
-const OrgPSPUserId = 'appOrgPSPUser13';
-
 const orgACDMSP = 'ACDMSP';
-const OrgACDUserId = 'appOrgACDUser13';
+const OrgACDUserId = 'appOrgACDUser15';
 
 const orgAADMSP = 'AADMSP';
-const OrgAADUserId = 'appOrgAADUser13';
+const OrgAADUserId = 'appOrgAADUser15';
 
 const orgAODMSP = 'AODMSP';
-const OrgAODUserId = 'appOrgAODUser13';
+const OrgAODUserId = 'appOrgAODUser15';
 
 const RED = '\x1b[31m\n';
 const GREEN = '\x1b[32m\n';
@@ -75,61 +69,56 @@ async function initGatewayForOrg(useCommitEvents, pathStr, caHostName, walletPat
 	}
 }
 
+let contractSubmitSettlementTxCCOrgACD,
+	contractAuthorizeSettlementTxCCOrgACD,
+	contractAuthorizeSettlementTxCCOrgAAD,
+	contractBalanceSettlementTxCCOrgAAD,
+	contractBalanceSettlementTxCCOrgAOD,
+	contractClearSettlementTxCCOrgAOD,
+	contractClearSettlementTxCCOrgAAD;
 
-
-io.on('connection', async (socket) => {
-	console.log('Client connected');
-	console.log(`${BLUE} **** START ****${RESET}`);
+const initialize = async (callback) => {
 	try {
-		const gateway1Org1 = await initGatewayForOrg(true, path.resolve(__dirname, '../../connection-org1.json'), 'ca_org1', path.join(__dirname, 'wallet', 'org1'), org1MSP, Org1UserId, 'org1.department1');
-		const gateway1OrgPSP = await initGatewayForOrg(true, path.resolve(__dirname, '../../connection-PSP.json'), 'ca_PSP', path.join(__dirname, 'wallet', 'orgPSP'), orgPSPMSP, OrgPSPUserId, 'orgPSP.department1');
 		const gateway1OrgACD = await initGatewayForOrg(true, path.resolve(__dirname, '../../connection-ACD.json'), 'ca_ACD', path.join(__dirname, 'wallet', 'orgACD'), orgACDMSP, OrgACDUserId, 'orgACD.department1');
 		const gateway1OrgAAD = await initGatewayForOrg(true, path.resolve(__dirname, '../../connection-AAD.json'), 'ca_AAD', path.join(__dirname, 'wallet', 'orgAAD'), orgAADMSP, OrgAADUserId, 'orgAAD.department1');
 		const gateway1OrgAOD = await initGatewayForOrg(true, path.resolve(__dirname, '../../connection-AOD.json'), 'ca_AOD', path.join(__dirname, 'wallet', 'orgAOD'), orgAODMSP, OrgAODUserId, 'orgAOD.department1');
+
+		const network1OrgACD = await gateway1OrgACD.getNetwork(channelName);
+		const network1OrgAAD = await gateway1OrgAAD.getNetwork(channelName);
+		const network1OrgAOD = await gateway1OrgAOD.getNetwork(channelName);
+
+
+		contractSubmitSettlementTxCCOrgACD = network1OrgACD.getContract("SubmitSettlementTxCC");
+		contractAuthorizeSettlementTxCCOrgACD = network1OrgACD.getContract("AuthorizeSettlementTxCC");
+		contractAuthorizeSettlementTxCCOrgAAD = network1OrgAAD.getContract("AuthorizeSettlementTxCC");
+		contractBalanceSettlementTxCCOrgAAD = network1OrgAAD.getContract("BalanceSettlementTxCC");
+		contractBalanceSettlementTxCCOrgAOD = network1OrgAOD.getContract("BalanceSettlementTxCC");
+		contractClearSettlementTxCCOrgAOD = network1OrgAOD.getContract("ClearSettlementTxCC");
+		contractClearSettlementTxCCOrgAAD = network1OrgAAD.getContract("ClearSettlementTxCC");
+
+		callback();
+	} catch (error) {
+		console.error(`Error in setup: ${error}`);
+		if (error.stack) {
+			console.error(error.stack);
+		}
+		process.exit(1);
+	}
+}
+
+const startServer = () => {
+	io.on('connection', async (socket) => {
+		console.log('Client connected');
+		console.log(`${BLUE} **** START ****${RESET}`);
+
 		try {
 			console.log(`${BLUE} **** CHAINCODE EVENTS ****${RESET}`);
 			let transaction;
-			const network1Org1 = await gateway1Org1.getNetwork(channelName);
-			const network1OrgPSP = await gateway1OrgPSP.getNetwork(channelName);
-			const network1OrgACD = await gateway1OrgACD.getNetwork(channelName);
-			const network1OrgAAD = await gateway1OrgAAD.getNetwork(channelName);
-			const network1OrgAOD = await gateway1OrgAOD.getNetwork(channelName);
-			const contractPYMTUTILSCCOrg1 = network1Org1.getContract("PYMTUtilsCC");
-			const contractSubmitSettlementTxCCOrgPSP = network1OrgPSP.getContract("SubmitSettlementTxCC");
-			const contractSubmitSettlementTxCCOrgACD = network1OrgACD.getContract("SubmitSettlementTxCC");
-			const contractAuthorizeSettlementTxCCOrgACD = network1OrgACD.getContract("AuthorizeSettlementTxCC");
-			const contractAuthorizeSettlementTxCCOrgAAD = network1OrgAAD.getContract("AuthorizeSettlementTxCC");
-			const contractBalanceSettlementTxCCOrgAAD = network1OrgAAD.getContract("BalanceSettlementTxCC");
-			const contractBalanceSettlementTxCCOrgAOD = network1OrgAOD.getContract("BalanceSettlementTxCC");
-			const contractClearSettlementTxCCOrgAOD = network1OrgAOD.getContract("ClearSettlementTxCC");
-			const contractClearSettlementTxCCOrgAAD = network1OrgAAD.getContract("ClearSettlementTxCC");
 
-			try {
-				// let listenerOrg1 = async (event) => {
-				// 	const stateObj = JSON.parse(event.payload.toString());
-				// 	console.log(`${stateObj.channelName}, ${stateObj.key}`);
-				// 	console.log(`${GREEN}<-- Contract Event Received: ${event.eventName} - ${stateObj} - ${JSON.stringify(stateObj)}${RESET}`);
-				// 	console.log(`*** Event: ${event.eventName}`);
-				// 	try {
-				// 		if (event.eventName == 'EMT-RT') {
-				// 			console.log(`${GREEN}--> Submit SubmitSettlementTxCC Transaction submitSettlementTx, ${stateObj.key}`);
-				// 			transaction = contractSubmitSettlementTxCCOrgPSP.createTransaction('submitSettlementTx');
-				// 			const splitKey = [...stateObj.key.split("-")];
-				// 			socket.emit('status-change', { status: 'requested', data: splitKey });
-				// 			console.log("Emitted status-change for requested - ", splitKey);
-				// 			await transaction.submit(...splitKey);
-				// 			console.log(`${GREEN}<-- Submit SubmitSettlementTxCC submitSettlementTx Result: committed, for ${stateObj.key}${RESET}`);
-				// 		}
-				// 	} catch (error) {
-				// 		console.log(`${RED}<-- Submit Failed: SubmitSettlementTxCC verifyAndChangeStatus - ${createError}${RESET}`);
-				// 	}
-				// };
-				// // now start the client side event service and register the listener
-				// console.log(`${GREEN}--> Start contract event stream to peer in Org1${RESET}`);
-				// await contractPYMTUTILSCCOrg1.addContractListener(listenerOrg1);
 
 
 
+			try {
 
 				let listenerOrgACD = async (event) => {
 					const stateObj = JSON.parse(event.payload.toString());
@@ -141,7 +130,7 @@ io.on('connection', async (socket) => {
 							console.log(`${GREEN}--> Submit AuthorizeSettlementTxCC Transaction authorizeSettlementTx, ${stateObj.key}`);
 							transaction = contractAuthorizeSettlementTxCCOrgACD.createTransaction('authorizeSettlementTx');
 							const splitKey = [...stateObj.key.split("-")];
-							socket.emit('status-change', { status: 'TxSubmitted', data: splitKey });
+							socket.emit('status-change', { status: 'TxSubmitted', data: splitKey, message: 'ACD has Verified attribute: Processing Code' });
 							console.log("Emitted status-change for TxSubmitted - ", { status: 'TxSubmitted', data: splitKey });
 							await transaction.submit(...splitKey);
 							console.log(`${GREEN}<-- Authorize AuthorizeSettlementTxCC authorizeSettlementTx Result: committed, for ${stateObj.key}${RESET}`);
@@ -165,7 +154,7 @@ io.on('connection', async (socket) => {
 							console.log(`${GREEN}--> Submit BalanceSettlementTxCC Transaction balanceSettlementTx, ${stateObj.key}`);
 							transaction = contractBalanceSettlementTxCCOrgAAD.createTransaction('balanceSettlementTx');
 							const splitKey = [...stateObj.key.split("-")];
-							socket.emit('status-change', { status: 'TxAuthorized', data: splitKey });
+							socket.emit('status-change', { status: 'TxAuthorized', data: splitKey, message: 'AAD has Verified attribute: Transaction Amount & AOD has verified attribute: System Trace Audit Number' });
 							console.log("Emitted status-change for TxAuthorized - ", { status: 'TxAuthorized', data: splitKey });
 							await transaction.submit(...splitKey);
 							console.log(`${GREEN}<-- Submit BalanceSettlementTxCC balanceSettlementTx Result: committed, for ${stateObj.key}${RESET}`);
@@ -189,7 +178,7 @@ io.on('connection', async (socket) => {
 							transaction = contractClearSettlementTxCCOrgAOD.createTransaction('clearSettlementTx');
 							const splitKey = [...stateObj.key.split("-")];
 
-							socket.emit('status-change', { status: 'TxBalanced', data: splitKey });
+							socket.emit('status-change', { status: 'TxBalanced', data: splitKey, message: 'AOD has Verified attribute: Point of Service Entry Mode & ACD has verified attribute: Merchant Category Code' });
 							console.log("Emitted status-change for TxBalanced - ", { status: 'TxBalanced', data: splitKey });
 							await transaction.submit(...splitKey);
 							console.log(`${GREEN}<-- Submit ClearSettlementTxCC clearSettlementTx Result: committed, for ${stateObj.key}${RESET}`);
@@ -211,7 +200,7 @@ io.on('connection', async (socket) => {
 					try {
 						if (event.eventName == 'EACDAAD-CT') {
 							const splitKey = [...stateObj.key.split("-")];
-							socket.emit('status-change', { status: 'TxCleared', data: splitKey });
+							socket.emit('status-change', { status: 'TxCleared', data: splitKey, message: 'AAD has Verified attribute: Acquiring Institution Identification Code & ACD has Verified attribute: Retrieval Reference Number' });
 							console.log("Emitted status-change for TxCleared - ", { status: 'TxCleared', data: splitKey });
 						}
 					} catch (error) {
@@ -230,15 +219,12 @@ io.on('connection', async (socket) => {
 				console.error(runError.stack);
 			}
 		}
-	} catch (error) {
-		console.error(`Error in setup: ${error}`);
-		if (error.stack) {
-			console.error(error.stack);
-		}
-		process.exit(1);
-	}
-});
 
-server.listen(3001, () => {
-	console.log("Server is listening on port 3001...");
-});
+	});
+
+	server.listen(3001, () => {
+		console.log("Server is listening on port 3001...");
+	});
+}
+
+initialize(startServer);
