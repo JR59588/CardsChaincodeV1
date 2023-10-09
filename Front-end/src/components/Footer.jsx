e136f716bee8397f22edf4900ab7822664a6f54a
azahar18 <mohammedazahar.shaik@zensar.com>
2023-10-09 12:36:53 +0530

    aligning footer text

--- Front-end/src/components/Footer.jsx
+++ Front-end/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import image from '../assets/logo3.png'
 const Footer = () => {
   return (
     <div className="bg mt-4" style={{ width:'auto', height: "70px", backgroundColor: "#10005d" }}>
-      <h4 style={{ textAlign: "center", lineHeight: "70px" ,color:'white' }}>
+      <h4 style={{ textAlign: "center", lineHeight: "70px" ,color:'white',marginLeft:'185px' }}>
         Developed by Blockchain Labs@Zensar
         <img style={{width:"120px", height:"70px", float:"right"}}  src={image} alt="no Img"/>
       </h4>
