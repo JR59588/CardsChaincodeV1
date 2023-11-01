be86a8e567a3fab821f6ea45d742539f598ff7ac
abhiroop99z <abhiroop.b@zensar.com>
2023-11-01 12:54:58 +0530

    dynamic navbar options

--- Front-end/src/components/Header.jsx
+++ Front-end/src/components/Header.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../App.css";
-import { useState } from "react";
-import { useEffect } from "react";
-import pdf from "../assets/Admin_User_guide.pdf";
+import navLinksData from "../assets/navbarOptions.json";
 
 const Header = ({ roleId, setRoleId, orgOptions }) => {
   return (
@@ -27,152 +25,73 @@ const Header = ({ roleId, setRoleId, orgOptions }) => {
           aria-controls="navbarText"
           aria-expanded="false"
           aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        />
         <div className="collapse navbar-collapse" id="navbarText">
           <ul
             style={{ display: "flex", alignItems: "center" }}
             className="navbar-nav me-auto mb-2 mb-lg-0"
           >
-            <li className="nav-item">
-              <NavLink to="/About" className="ani">
-                About
-              </NavLink>
-            </li>
-            {/* <li className="nav-item">
-              <NavLink to="/Aggregator" className="ani">
-                New Onboarding{" "}
-                <span className="gapLine" style={{ color: "white" }}>
-                  |
-                </span>
-              </NavLink>
-            </li>
+            {navLinksData.map((el, i) => {
+              if (!el.children) {
+                return (
+                  <li key={el.id} className="nav-item">
+                    <NavLink to={el.url} className="ani">
+                      {el.name}
+                    </NavLink>
+                  </li>
+                );
+              }
 
-            <li className="nav-item">
-              <NavLink to="/View-Onboarding-Static" className="ani">
-                View Onboarding{" "}
-                <span className="gapLine" style={{ color: "white" }}>
-                  |
-                </span>
-              </NavLink>
-            </li> */}
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-                style={{ color: "white" }}
-              >
-                Onboarding
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <NavLink className="dropdown-item" to="/Aggregator">
-                  Add new merchant
-                </NavLink>
-                <NavLink className="dropdown-item" to="/View-Onboarding-Static">
-                  View onboarded merchant
-                </NavLink>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-                style={{ color: "white" }}
-              >
-                Settlements
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <NavLink className="dropdown-item" to="/Merchant">
-                  Submit new request
-                </NavLink>
-                <NavLink className="dropdown-item" to="/ViewTx">
-                  View submitted request
-                </NavLink>
-              </div>
-            </li>
-            {/* <li className="nav-item">
-              <NavLink to="/Merchant" className="ani">
-                Settlement Request{" "}
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/ViewTx" className="ani">
-                View Settlement Requests{" "}
-              </NavLink>{" "}
-            </li> */}
-            <li className="nav-item">
-              <NavLink to="/DashboardSummary" className="ani">
-                Dashboard{" "}
-              </NavLink>{" "}
-            </li>
-          </ul>
-          <div className="d-flex">
-            <div style={{ display: "flex" }}>
-              <label
-                htmlFor="dropdown"
-                style={{ color: "white", lineHeight: "35px" }}
-                className="dropLable"
-              >
-                Role*: &nbsp;
-              </label>
-              <select
-                style={{
-                  width: "210px",
-                  backgroundColor: "#10005d",
-                  height: "36px",
-                  color: "white",
-                }}
-                className="form-select"
-                id="dropdown"
-                required
-                value={roleId}
-                onChange={(event) => setRoleId(event.target.value)}
-              >
-                {orgOptions.map((orgOption, index) => (
-                  <option
-                    value={orgOption.orgId}
-                    id={orgOption.orgId}
-                    key={"org" + index}
-                    className="dropdown-item"
+              return (
+                <li key={el.id} class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                    style={{ color: "white" }}
                   >
-                    {orgOption.orgName}
-                  </option>
-                ))}
-                {/* <option value="AP" id="AP" className="dropdown-item">
-                  Select Role
-                </option>
-                <option value="Org1" id="Org1" className="dropdown-item">
-                  Merchant Org 1
-                </option>
-                <option value="Org2" id="Org2" className="dropdown-item">
-                  Merchant Org 2
-                </option>
-                <option value="PSP" id="PSP" className="dropdown-item">
-                  Payment Service Provider
-                </option>
-                <option value="ACD" id="ACD" className="dropdown-item">
-                  Acquirer Customer Department
-                </option>
-                <option value="AAD" id="AAD" className="dropdown-item">
-                  Acquirer Accounts Department
-                </option>
-                <option value="AOD" id="AOD" className="dropdown-item">
-                  Acquirer Operations Department
-                </option> */}
-              </select>
-            </div>
-          </div>
+                    {el.name}
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    {el.children.map((ele) => (
+                      <NavLink className="dropdown-item" to={ele.url}>
+                        {ele.name}
+                      </NavLink>
+                    ))}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+
+          <select
+            style={{
+              width: "210px",
+              backgroundColor: "#10005d",
+              height: "36px",
+              color: "white",
+            }}
+            className="form-select"
+            id="dropdown"
+            required
+            value={roleId}
+            onChange={(event) => setRoleId(event.target.value)}
+          >
+            {orgOptions.map((orgOption, index) => (
+              <option
+                value={orgOption.orgId}
+                id={orgOption.orgId}
+                key={"org" + index}
+                className="dropdown-item"
+              >
+                {orgOption.orgName}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
     </nav>
