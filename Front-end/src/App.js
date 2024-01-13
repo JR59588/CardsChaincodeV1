9d2e733c38834d004bcdc1c7cb37ea540992b8b1
abhiroop99z <abhiroop.b@zensar.com>
2024-01-13 20:22:22 +0530

    inital changes for merchant onboarding page

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,39 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { Routes } from 'react-router-dom'
-import FileComponent from './components/Merchant-Submit-Deails/FileComponent'
-import Header from './components/Header'
-import Merchant from './components/Merchant'
-import { Route } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Routes } from "react-router-dom";
+import FileComponent from "./components/Merchant-Submit-Deails/FileComponent";
+import Header from "./components/Header";
+import Merchant from "./components/Merchant";
+import { Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import About from "./components/About";
 import DashboardSummary from "./components/DashboardSummary";
-import Onboard from './components/Onboarding/Onboard'
-import NewViewOnboardingStatic from './components/ViewOnboardingStatic/NewViewOnboardingStatic'
-import 'react-notifications/lib/notifications.css';
-import { NotificationContainer } from 'react-notifications';
-import ViewTx from './components/ViewTx'
-import socketIOClient from 'socket.io-client';
-import axios from 'axios'
-import ISO8583FormikComponent from './components/Merchant-Submit-Deails/ISO8583Formik/ISO8583FormikComponent'
-import ISO8583FileFormikComponent from './components/Merchant-Submit-Deails/ISO8583FileFormik/ISO8583FileFormikComponent'
-import Settlement from './components/SettlementRequests/Settlement'
-import Footer from './components/Footer'
-const IP = 'localhost'
-const socketEndpointUrl = 'http://localhost:3001';
+import Onboard from "./components/Onboarding/Onboard";
+import NewViewOnboardingStatic from "./components/ViewOnboardingStatic/NewViewOnboardingStatic";
+import "react-notifications/lib/notifications.css";
+import { NotificationContainer } from "react-notifications";
+import ViewTx from "./components/ViewTx";
+import socketIOClient from "socket.io-client";
+import axios from "axios";
+import ISO8583FormikComponent from "./components/Merchant-Submit-Deails/ISO8583Formik/ISO8583FormikComponent";
+import ISO8583FileFormikComponent from "./components/Merchant-Submit-Deails/ISO8583FileFormik/ISO8583FileFormikComponent";
+import Settlement from "./components/SettlementRequests/Settlement";
+import Footer from "./components/Footer";
+import OnboardFormik from "./components/Onboarding/OnboardFormik";
+const IP = "localhost";
+const socketEndpointUrl = "http://localhost:3001";
 
 function App() {
   //storing the roleId...
-  const [roleId, setRoleId] = useState('Org1')
+  const [roleId, setRoleId] = useState("Org1");
   const [orgOptions, setOrgOptions] = useState([]);
   const socket = socketIOClient(socketEndpointUrl);
 
   function fetchOrganizationData() {
     axios
-      .get(
-        `http://${IP}:3001/api/v1/getOrgs`
-      )
+      .get(`http://${IP}:3001/api/v1/getOrgs`)
       .then((response) => {
         setOrgOptions([...response.data.orgs]);
       })
@@ -56,9 +55,9 @@ function App() {
   }, [socket]);
 
   const getRoleFromFile = (roleState) => {
-    console.log('app', roleState)
-    setRoleId(roleState)
-  }
+    console.log("app", roleState);
+    setRoleId(roleState);
+  };
 
   return (
     <div>
@@ -78,38 +77,68 @@ function App() {
         />
         <Route
           path="/settlement-request"
+          element={<Settlement roleId={roleId} />}
+        />
+        <Route
+          path="/Aggregator"
           element={
-            <Settlement
+            <Onboard
               roleId={roleId}
+              IP={IP}
+              fetchOrganizationData={fetchOrganizationData}
             />
           }
         />
         <Route
-          path="/Aggregator"
-          element={<Onboard roleId={roleId} IP={IP} fetchOrganizationData={fetchOrganizationData} />}
+          path="/OnboardFormik"
+          element={
+            <OnboardFormik
+              roleId={roleId}
+              IP={IP}
+              fetchOrganizationData={fetchOrganizationData}
+            />
+          }
         />
         <Route
           path="/Onboard"
-          element={<Onboard roleId={roleId} IP={IP} fetchOrganizationData={fetchOrganizationData} />}
+          element={
+            <Onboard
+              roleId={roleId}
+              IP={IP}
+              fetchOrganizationData={fetchOrganizationData}
+            />
+          }
         />
         <Route
           path="/FileComponent"
           element={<FileComponent roleId={roleId} IP={IP} />}
         />
-        <Route path="/ViewTx" element={<ViewTx roleId={roleId} IP={IP} socket={socket} />} />
+        <Route
+          path="/ViewTx"
+          element={<ViewTx roleId={roleId} IP={IP} socket={socket} />}
+        />
         <Route
           path="/View-Onboarding-Static"
           element={<NewViewOnboardingStatic roleId={roleId} IP={IP} />}
         />
         <Route path="/About" element={<About />} />
-        <Route path="/isoformik" element={<ISO8583FormikComponent roleId={roleId} />} />
-        <Route path="/isofileformik" element={<ISO8583FileFormikComponent roleId={roleId} />} />
-        <Route path="/DashboardSummary" element={<DashboardSummary roleId={roleId} />} />
+        <Route
+          path="/isoformik"
+          element={<ISO8583FormikComponent roleId={roleId} />}
+        />
+        <Route
+          path="/isofileformik"
+          element={<ISO8583FileFormikComponent roleId={roleId} />}
+        />
+        <Route
+          path="/DashboardSummary"
+          element={<DashboardSummary roleId={roleId} />}
+        />
         <Route path="*" element={<h1>Not found....</h1>} />
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
